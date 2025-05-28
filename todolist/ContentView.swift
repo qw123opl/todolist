b6113cadd747fd3c4125dc331f38e0e1267b5626
hao <haotsehuang@ltedu.com.tw>
2025-05-28 22:09:33 +0800

    Initial Commit

---
+++ todolist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todolist
+//
+//  Created by 黃皓澤 on 2025/5/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by 黃皓澤 on 2025/5/28.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
